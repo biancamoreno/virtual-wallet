72e029577c5939572665b9fb714f6fc86eda96ee
Bia <bianca.araujo@cognizant.com>
2020-03-13 21:29:42 -0300

    button component && scss files configuration

---
+++ src/atoms/button/index.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import "../../assets/styles/css/Button.css";
+
+class Button extends React.Component {
+  handleClick = event => {
+    event.preventDefault()
+    this.props.handleClick()
+  }
+
+  render() {
+    return (
+      <button
+        type="button"
+        className={"btn " + this.props.classes}
+        onClick={this.handleClick}
+        disabled={this.props.disabled}
+      >
+        {this.props.label}
+      </button>
+    )
+  }
+}
+
+export default Button
